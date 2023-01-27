5c1187c0301b1f89f54ace974e88b85b59ee343d
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-01-27 09:09:08 -0300

    mais sobre heranca

---
+++ src/polimorfismo_heranca_interface/bytebank_heranca_conta/src/Conta.java
@@ -0,0 +1,72 @@
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular = new Cliente();
+	private static int quantidadeDeContas;
+
+	public Conta(int agencia, int numero) {
+		if (agencia >= 0 && numero >= 0) {
+			System.out.println("Estou criando uma conta..");
+			quantidadeDeContas++;
+
+			this.agencia = agencia;
+			this.numero = numero;
+		} else {
+			System.out.println("Valores inválidos.");
+		}
+
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public static int getQuantidadeDeContas() {
+		return Conta.quantidadeDeContas;
+	}
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+		System.out.println("Valor depositado: R$" + valor);
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			System.out.println("Saque de R$" + valor);
+			return true;
+		} else {
+			System.out.println("Não foi possível realizar o saque. Saldo indisponível.");
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			System.out.println("Transferência realizada.");
+			destino.deposita(valor);
+			return true;
+		} else {
+			System.out.println("Não foi possível realizar a transferência. Saldo indisponível.");
+			return false;
+		}
+	}
+}
---
+++ src/polimorfismo_heranca_interface/bytebank_heranca_conta/src/ContaCorrente.java
@@ -0,0 +1,10 @@
+package polimorfismo_heranca_interface.bytebank_heranca_conta.src;
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia, numero);
+		// TODO Auto-generated constructor stub
+	}
+
+}
