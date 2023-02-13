d0c94ad26961bc1593ffbfe7c420b4f907cb6d7e
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-02-13 14:20:08 -0300

    type casting

--- src/polimorfismo_heranca_interface/bytebank_heranca_conta/src/br/com/bytebank/banco/teste/TestaArrayReferencia.java
+++ src/polimorfismo_heranca_interface/bytebank_heranca_conta/src/br/com/bytebank/banco/teste/TestaArrayReferencia.java
@@ -1,21 +1,24 @@
 package polimorfismo_heranca_interface.bytebank_heranca_conta.src.br.com.bytebank.banco.teste;
 
+import polimorfismo_heranca_interface.bytebank_heranca_conta.src.br.com.bytebank.banco.modelo.Conta;
 import polimorfismo_heranca_interface.bytebank_heranca_conta.src.br.com.bytebank.banco.modelo.ContaCorrente;
+import polimorfismo_heranca_interface.bytebank_heranca_conta.src.br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TestaArrayReferencia {
 	public static void main(String[] args) {
 
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Conta[] contas = new Conta[5];
+
 		ContaCorrente cc1 = new ContaCorrente(4638, 9933);
 		contas[0] = cc1;
 
-		ContaCorrente cc2 = new ContaCorrente(4638, 9389);
-		contas[1] = cc2;
+		ContaPoupanca cp1 = new ContaPoupanca(4638, 9389);
+		contas[1] = cp1;
 
 		System.out.println(contas[0].getNumero());
 
-		int[] refs = { 1, 2, 3, 4, 5 };
-		System.out.println(refs[3]);
+		ContaPoupanca ref = (ContaPoupanca) contas[1];
+		System.out.println(ref.getNumero());
 
 	}
 }
