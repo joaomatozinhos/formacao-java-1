53af3ae6511a95d9ba822db71850f2616884fc0c
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-02-13 13:47:09 -0300

    array de referencias

---
+++ src/polimorfismo_heranca_interface/bytebank_heranca_conta/src/br/com/bytebank/banco/teste/TestaArrayReferencia.java
@@ -0,0 +1,18 @@
+package polimorfismo_heranca_interface.bytebank_heranca_conta.src.br.com.bytebank.banco.teste;
+
+import polimorfismo_heranca_interface.bytebank_heranca_conta.src.br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TestaArrayReferencia {
+	public static void main(String[] args) {
+
+		ContaCorrente[] contas = new ContaCorrente[5];
+		ContaCorrente cc1 = new ContaCorrente(4638, 9933);
+		contas[0] = cc1;
+
+		ContaCorrente cc2 = new ContaCorrente(4638, 9389);
+		contas[1] = cc2;
+
+		System.out.println(contas[0].getNumero());
+
+	}
+}
