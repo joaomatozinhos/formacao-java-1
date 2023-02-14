64f146e214c2721619706ed31aa13f91dddcdd38
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-02-14 08:24:43 -0300

    string[] args

---
+++ src/polimorfismo_heranca_interface/bytebank_heranca_conta/src/br/com/bytebank/banco/teste/TestaArrayString.java
@@ -0,0 +1,12 @@
+package polimorfismo_heranca_interface.bytebank_heranca_conta.src.br.com.bytebank.banco.teste;
+
+public class TestaArrayString {
+	public static void main(String[] args) {
+
+		System.out.println("Funcionou!!");
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+}
