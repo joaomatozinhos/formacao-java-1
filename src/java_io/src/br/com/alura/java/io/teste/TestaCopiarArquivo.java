df0d168ba71f194452e07489cfcc155b70a1620e
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-03-02 06:33:52 -0300

    outras entradas e saidas

--- src/java_io/src/br/com/alura/java/io/teste/TestaCopiarArquivo.java
+++ src/java_io/src/br/com/alura/java/io/teste/TestaCopiarArquivo.java
@@ -2,7 +2,6 @@ package java_io.src.br.com.alura.java.io.teste;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,19 +15,21 @@ public class TestaCopiarArquivo {
 
 	public static void main(String[] args) throws IOException {
 
-		InputStream fis = new FileInputStream("lorem.txt");
+		InputStream fis = System.in; // new FileInputStream("lorem.txt");
 		Reader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 
-		OutputStream fos = new FileOutputStream("lorem2.txt");
+		OutputStream fos = System.out; // new FileOutputStream("lorem2.txt");
 		Writer osw = new OutputStreamWriter(fos);
 		BufferedWriter bw = new BufferedWriter(osw);
 
 		String linha = br.readLine();
 
-		while (linha != null) {
+		while (linha != null && !linha.isEmpty()) {
+
 			bw.write(linha);
 			bw.newLine();
+			bw.flush();
 			linha = br.readLine();
 		}
 
