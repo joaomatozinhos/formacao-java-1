0f91645eced378b5f23e5212055e54f6c67914e0
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-03-06 07:39:00 -0300

    leitura com Scanner

---
+++ src/java_io/src/br/com/alura/java/io/teste/TestaLeituraScanner.java
@@ -0,0 +1,19 @@
+package java_io.src.br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TestaLeituraScanner {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner scanner = new Scanner(new File("contas.csv"));
+
+		while (scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+			System.out.println(linha);
+		}
+
+		scanner.close();
+	}
+}
