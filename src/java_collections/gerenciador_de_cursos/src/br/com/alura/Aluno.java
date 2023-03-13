20a754fff81b03114743c428481255702c2f1345
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-03-13 07:50:34 -0300

    uso de Set conjuntos

---
+++ src/java_collections/gerenciador_de_cursos/src/br/com/alura/Aluno.java
@@ -0,0 +1,25 @@
+package java_collections.gerenciador_de_cursos.src.br.com.alura;
+
+public class Aluno {
+
+	private String nome;
+	private int numeroMatricula;
+
+	public Aluno(String nome, int numeroMatricula) {
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getNumeroMatricula() {
+		return numeroMatricula;
+	}
+
+	@Override
+	public String toString() {
+		return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+	}
+}
--- src/java_collections/gerenciador_de_cursos/src/br/com/alura/Curso.java
+++ src/java_collections/gerenciador_de_cursos/src/br/com/alura/Curso.java
@@ -2,12 +2,15 @@ package java_collections.gerenciador_de_cursos.src.br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -28,5 +31,13 @@ public class Curso {
 
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
 	}
 }
