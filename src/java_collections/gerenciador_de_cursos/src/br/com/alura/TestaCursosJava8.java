bd6a7d6f23fd27267ef46511075927e23f9c89c7
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-03-21 07:38:03 -0300

    gerando uma coleção a partir de um Stream; classe Optional

--- src/java_collections/gerenciador_de_cursos/src/br/com/alura/TestaCursosJava8.java
+++ src/java_collections/gerenciador_de_cursos/src/br/com/alura/TestaCursosJava8.java
@@ -3,6 +3,10 @@ package java_collections.gerenciador_de_cursos.src.br.com.alura;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestaCursosJava8 {
 
@@ -18,6 +22,8 @@ public class TestaCursosJava8 {
 		cursos.stream()
 			.filter(c -> c.getQuantidadeDeAlunos() >= 100)
 			.forEach(c -> System.out.println(c.getNome()));
+		
+		System.out.println("==========");
 
 		cursos.stream()
 			.filter(c -> c.getQuantidadeDeAlunos() >= 100)
@@ -25,6 +31,8 @@ public class TestaCursosJava8 {
 			.map(Curso::getQuantidadeDeAlunos)
 //			.forEach(total -> System.out.println(total));
 			.forEach(System.out::println);
+		
+		System.out.println("==========");
 
 		int sum = cursos.stream()
 			.filter(c -> c.getQuantidadeDeAlunos() >= 100)
@@ -32,5 +40,48 @@ public class TestaCursosJava8 {
 			.mapToInt(Curso::getQuantidadeDeAlunos).sum();
 
 		System.out.println("Soma: " + sum);
+		
+		System.out.println("==========");
+		
+        OptionalDouble average = cursos.stream()
+        	.mapToInt(Curso::getQuantidadeDeAlunos)
+            .average();
+
+        System.out.println(average);
+        
+        System.out.println("==========");
+		
+        cursos.stream()
+        	.filter(c -> c.getQuantidadeDeAlunos() >= 100)
+        	.findFirst()
+        	.ifPresent(c -> System.out.println(c.getNome()));
+		
+		System.out.println("==========");
+		
+		Stream<String> nomes = cursos.stream().map(Curso::getNome);
+		nomes.forEach(c -> System.out.println(c));
+		
+		System.out.println("==========");
+		
+		cursos.stream()
+		   .filter(c -> c.getQuantidadeDeAlunos() >= 100)
+		   .findAny()
+		   .ifPresent(c -> System.out.println(c.getNome()));
+		
+		System.out.println("==========");
+		
+		cursos = cursos.stream()
+			.filter(c -> c.getQuantidadeDeAlunos() >= 100)
+			.collect(Collectors.toList());
+	
+		cursos.forEach(c -> System.out.println(c.getNome()));
+		
+		System.out.println("==========");
+		
+		Map<String, Integer> mapa = cursos.stream() 
+			.filter(c -> c.getQuantidadeDeAlunos() > 100) 
+			.collect(Collectors.toMap(c -> c.getNome(), c -> c.getQuantidadeDeAlunos()));
+		
+		mapa.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
 	}
 }
