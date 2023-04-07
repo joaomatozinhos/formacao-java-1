120a5e2047b1ce5188b18e433d1349daa7f4ab2b
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-04-07 15:23:27 -0300

    inicio projeto screen match

---
+++ src/projeto_screen_match/Main.java
@@ -0,0 +1,27 @@
+package projeto_screen_match;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Esse é o Screen Match");
+		System.out.println("Filme: Top Gun: Maverick");
+
+		int anoDeLancamento = 2022;
+		boolean incluidoNoPlano = true;
+		double notaDoFilme = 8.1;
+
+		System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+		double media = (9.8 + 6.3 + 8) / 3;
+		System.out.println(media);
+
+		String sinopse;
+		sinopse = """
+				Filme Top Gun
+				Filme de aventura com galã dos anos 80
+				Muito bom!
+				Ano de Lançamento
+				""" + anoDeLancamento;
+		System.out.println(sinopse);
+
+	}
+}
