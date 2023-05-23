6a6dfbc8c97a34e96d1a729689bced0c61a406be
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-05-23 07:29:16 -0300

    codigo de registro de orcamento

---
+++ src/design_patterns/loja/src/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,9 @@
+package design_patterns.loja.src.br.com.alura.loja.orcamento;
+
+public class RegistroDeOrcamento {
+	
+	public void registra(Orcamento orcamento) {
+		// chamada HTTP para a API externa
+	}
+
+}
