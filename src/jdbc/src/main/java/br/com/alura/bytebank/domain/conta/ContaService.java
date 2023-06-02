a5faf18202dd680d2447fc392bd4d84f4296e93f
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-06-02 08:53:25 -0300

    insert no banco de dados

--- src/jdbc/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ src/jdbc/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -1,15 +1,24 @@
 package jdbc.src.main.java.br.com.alura.bytebank.domain.conta;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
+import jdbc.src.main.java.br.com.alura.bytebank.ConnectionFactory;
 import jdbc.src.main.java.br.com.alura.bytebank.domain.RegraDeNegocioException;
 import jdbc.src.main.java.br.com.alura.bytebank.domain.cliente.Cliente;
 
 public class ContaService {
 
+	private ConnectionFactory connection;
 	private Set<Conta> contas = new HashSet<>();
+
+	public ContaService() {
+		this.connection = new ConnectionFactory();
+	}
 
 	public Set<Conta> listarContasAbertas() {
 		return contas;
@@ -27,7 +36,28 @@ public class ContaService {
 			throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
 		}
 
-		contas.add(conta);
+		String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)"
+				+ "VALUES (?, ?, ?, ?, ?)";
+
+		Connection con = connection.recuperaConexao();
+
+		try {
+			PreparedStatement preparedStatement = con.prepareStatement(sql);
+
+			preparedStatement.setInt(1, conta.getNumero());
+			preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
+			preparedStatement.setString(3, dadosDaConta.dadosCliente().nome());
+			preparedStatement.setString(4, dadosDaConta.dadosCliente().cpf());
+			preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
+
+			preparedStatement.execute();
+
+			preparedStatement.close();
+			con.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
