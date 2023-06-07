29ece43e5de67bf4c144324cec03220fa0c30aff
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2023-06-07 08:01:14 -0300

    escalabilidade de conexoes (pool de conexoes)

--- src/jdbc/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/jdbc/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,17 +1,28 @@
 package jdbc.src.main.java.br.com.alura.bytebank;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectionFactory {
 
 	public Connection recuperaConexao() {
 		try {
-			return DriverManager
-					.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=root");
+			return createDataSource().getConnection();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private HikariDataSource createDataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+		config.setUsername("root");
+		config.setPassword("tihhed-mifpuG-7padze");
+		config.setMaximumPoolSize(10);
+
+		return new HikariDataSource(config);
 	}
 }
